dd9a9fa550ee6cda33ecc6acbd09c0c31deb82ec
miguelmendez <miguelmendez1208@gmail.com>
2024-10-05 18:20:56 -0500

    change the command to src/MyNFTFactory.sol:MyNFTFactory. Created NFT and NFT factory.

---
+++ src/MyNFT.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721, Ownable {
+    uint256 private _nextTokenId;
+
+    constructor(address initialOwner)
+        ERC721("MyToken", "MTK")
+        Ownable(initialOwner)
+    {}
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+    }
+}
