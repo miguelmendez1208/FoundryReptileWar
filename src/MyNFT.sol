ed9437579c69d0582e7cbd3f80fabb324f516035
miguelmendez <miguelmendez1208@gmail.com>
2024-10-05 19:34:37 -0500

    deleted NFT factory. Changed MyNFT.sol. updated read me. changed environment variables.

--- src/MyNFT.sol
+++ src/MyNFT.sol
@@ -1,16 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
-import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
-import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyNFT is ERC721, Ownable {
     uint256 private _nextTokenId;
 
-    constructor(address initialOwner)
-        ERC721("MyToken", "MTK")
-        Ownable(initialOwner)
-    {}
+    constructor(address initialOwner) ERC721("ReptileTrophy", "RTK")
+ Ownable(initialOwner) {
+        // The Ownable constructor automatically sets the owner to msg.sender
+        _nextTokenId = 0;
+    }
 
     function safeMint(address to) public onlyOwner {
         uint256 tokenId = _nextTokenId++;
